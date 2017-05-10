518b5edc2ad433e07999d4c63b7c5b8c4f29cf8f
Fabricio Salazar <salazar.fabricio@gmail.com>
2017-05-09 19:33:19 -0600

    Primer commit - proyecto vacío

---
+++ src/main/java/com/cr/ac/ucr/lenguajes/j2fshop/J2FShopApplication.java
@@ -0,0 +1,12 @@
+package com.cr.ac.ucr.lenguajes.j2fshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2FShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J2FShopApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cr/ac/ucr/lenguajes/j2fshop/J2FShopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cr.ac.ucr.lenguajes.j2fshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class J2FShopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
