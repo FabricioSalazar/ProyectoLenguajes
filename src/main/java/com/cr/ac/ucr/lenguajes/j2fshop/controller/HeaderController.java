33962bcb635c70341aa5b054d61aa04fd07e4257
Juan Carlos <jcarlosmc1994@gmail.com>
2017-05-11 08:52:16 -0600

    Se agrego el header.html el cual puede ser agregado en cualquier html con la funcion <iframe> (Existe un ejemplo en el html J2FShop)

---
+++ src/main/java/com/cr/ac/ucr/lenguajes/j2fshop/controller/HeaderController.java
@@ -0,0 +1,15 @@
+package com.cr.ac.ucr.lenguajes.j2fshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HeaderController {
+
+	@RequestMapping("header"  )
+	public String iniciar(Model model) {
+		return "header";
+	}
+		
+}
---
+++ src/main/java/com/cr/ac/ucr/lenguajes/j2fshop/controller/J2fShopController.java
@@ -0,0 +1,15 @@
+package com.cr.ac.ucr.lenguajes.j2fshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class J2fShopController {
+
+	@RequestMapping("/J2FShop"  )
+	public String iniciar(Model model) {
+		return "J2FShop";
+	}
+	
+}
