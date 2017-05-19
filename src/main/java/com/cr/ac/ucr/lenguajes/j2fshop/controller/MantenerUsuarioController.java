a23556fa7145f780c44bed62e4d1d97d5addad26
Fabricio Salazar <salazar.fabricio@gmail.com>
2017-05-19 16:28:23 -0600

    mantenimiento de usuarios, administrador parcial

--- src/main/java/com/cr/ac/ucr/lenguajes/j2fshop/controller/MantenerUsuarioController.java
+++ src/main/java/com/cr/ac/ucr/lenguajes/j2fshop/controller/MantenerUsuarioController.java
@@ -1,10 +1,14 @@
 package com.cr.ac.ucr.lenguajes.j2fshop.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -20,7 +24,14 @@ public class MantenerUsuarioController {
 	@Autowired
 	UsuarioService usuarioService;
 	
-	//Show user info
+	//User maintenance, admin view
+	@RequestMapping(value = "/admin/mantenerUsuarios", method = RequestMethod.GET)
+	public String mantenerUsuarios(Model model) {
+		model.addAttribute("users", usuarioService.findAllUsers());
+		return "mantenerUsuarios";
+	}
+
+	//Modify, show user info
 	@RequestMapping(value = "/modificarUsuario", method = RequestMethod.GET)
 	public String modifyUserView(UsuarioForm usuarioForm, Model model) {
 		Usuario user = usuarioService.findUserByLogIn(SecurityContextHolder.getContext().getAuthentication().getName());
@@ -29,7 +40,7 @@ public class MantenerUsuarioController {
 		return "modificarUsuario";
 	}
 	
-	//Save new user info
+	//Modify, save new user info
 	@RequestMapping(value = "/modificarUsuario", method = RequestMethod.POST)
 	public String modifyUserAction(@Valid UsuarioForm usuarioForm,
 			BindingResult bindingResult, Model model) {
@@ -69,4 +80,46 @@ public class MantenerUsuarioController {
 		
 		return errors?"modificarUsuario":"success";
 	}
+
+	//"Erase" user view (disable)
+	@RequestMapping(value = "/borrarUsuario", method = RequestMethod.GET)
+	public String eraseUserView() {
+		return "erase";
+	}
+	
+	//"Erase" user (disable)
+	@RequestMapping(value = "/borrarUsuario", method = RequestMethod.POST)
+	public String eraseUserView(Model model, HttpServletRequest request, HttpServletResponse response) {
+		//Get the login
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+		
+		//Log out the current user
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){    
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+		
+        //disable the user
+        usuarioService.erase(username);
+        
+		model.addAttribute("msg");
+		return "login";
+	}
+	
+	//"Erase" user, admin view (disable)
+	@RequestMapping(value = "/admin/borrarUsuario", method = RequestMethod.GET)
+	public String eraseUserAdminView() {
+		return "erase";
+	}
+	
+	//"Erase" user, admin (disable)
+	@RequestMapping(value = "/admin/borrarUsuario", method = RequestMethod.POST)
+	public String eraseUserAdminView(HttpServletRequest request) {
+		
+		//disable the user
+        //usuarioService.erase(request.getAttribute("username").toString());
+        
+		return "mantenerUsuarios";
+	}
+	
 }
