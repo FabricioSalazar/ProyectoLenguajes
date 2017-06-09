4fe82dfefc6a546971782edede95c2a594d7bea4
Juan Carlos <jcarlosmc1994@gmail.com>
2017-06-08 18:16:21 -0600

    Mas ordenacion de codigo

--- src/main/java/com/cr/ac/ucr/lenguajes/j2fshop/controller/GeneralController.java
+++ src/main/java/com/cr/ac/ucr/lenguajes/j2fshop/controller/GeneralController.java
@@ -1,16 +1,7 @@
 package com.cr.ac.ucr.lenguajes.j2fshop.controller;
 
 
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.sql.Connection;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,28 +11,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import com.cr.ac.ucr.lenguajes.j2fshop.business.UsuarioService;
 import com.cr.ac.ucr.lenguajes.j2fshop.domain.Usuario;
+import com.cr.ac.ucr.lenguajes.j2fshop.storage.SessionManager;
 
-import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperExportManager;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.design.JasperDesign;
-import net.sf.jasperreports.engine.xml.JRXmlLoader;
-/*
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperExportManager;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.design.JasperDesign;
-import net.sf.jasperreports.engine.xml.JRXmlLoader;
-*/
+
 @Controller
 public class GeneralController {
 	@Autowired
@@ -50,10 +26,12 @@ public class GeneralController {
 	UsuarioService usuarioService;
 	
 	@RequestMapping(value = {"/", "/welcome", "/index"}, method = RequestMethod.GET)
-	public String home(Model model) {
+	public String home(Model model, HttpServletRequest request) {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		for(Object autho : auth.getAuthorities().toArray()){
 			System.out.println(autho.toString());
+			Usuario user = usuarioService.findUserByLogIn(auth.getName());
+			SessionManager.setSession(request.getSession().getId(), user);
 			if (autho.toString().equals("Administrador")){
 				model.addAttribute("admin",true);
 			}
